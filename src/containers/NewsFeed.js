ad38400b4673cc318b2a38631f20993c4c8e8a49
dhamodharan-baskar <damu.1394@gmail.com>
2018-10-30 01:51:24 +0530

    logout feature in news feed

--- src/containers/NewsFeed.js
+++ src/containers/NewsFeed.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 class NewsFeed extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      token:'',
+      newsList: []
+    }
+  }
+  async componentWillMount (){
+    let accessToken = await localStorage.getItem('access_token');
+    if (!accessToken) {
+      this.props.history.push('/login');
+    }
+    else {
+      this.setState({token: accessToken})
+    }
+  }
+
+  logout = async() => {
+    await localStorage.removeItem('access_token')
+    window.location.href = '/'
+  }
+  
   render() {
     return (
       <div>
-        feed
+        <div onClick={() => this.logout()}>
+         logout
+        </div>
       </div>
     );
   }
